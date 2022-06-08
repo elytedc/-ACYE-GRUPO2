1db914009295a8beb0a0ae3bc906d511a2e99fc2
elytedc <70500580+elytedc@users.noreply.github.com>
2022-06-08 00:05:49 -0600

    EEPROM
    EEPROM

--- sensores y ultraso/Arduino/Proyecto1/Proyecto1.ino
+++ sensores y ultraso/Arduino/Proyecto1/Proyecto1.ino
@@ -4,6 +4,7 @@
 #include <Stepper.h>
 
 
+
 // MATRIZ
 LedControl lc = LedControl(51, 52, 53, 1);
 
@@ -11,6 +12,13 @@ int TRIG = 30;
 int ECO = 31;
 int DURACION;
 int DISTANCIA;
+// variables EEPROM..............
+int cantidad_limon=0;
+int cantidad_vainilla=0;
+int cantidad_fresa=0;
+int cantidad_uva=0;
+int cantidad_napolitano=0;
+float costo=0;
 
 
 int bolas[3];
@@ -41,6 +49,7 @@ Stepper stepper(100, 8, 9, 10, 11);
 
 
 void setup() {
+
   Serial.begin(9600);
   // BUZZER
   pinMode(buzzer, OUTPUT);
@@ -60,12 +69,49 @@ void setup() {
   bolas[1] = 124;
   bolas[2] = 126;
  pinMode(TRIG, OUTPUT);
-  pinMode(ECO, INPUT);   
+ pinMode(ECO, INPUT);   
+//Serial.print("valor gua");
+//Serial.println(EEPROM.read(0));
+//Serial.print("leer");
+//EEPROM.write(0,45);
+
+/////////////////Guardar en memoria eeprom..............
+//EEPROM.put(0, cantidad_limon);
+//EEPROM.put(10, cantidad_vainilla);
+//EEPROM.put(20, cantidad_fresa);
+//EEPROM.put(30, cantidad_uva);
+//EEPROM.put(40, cantidad_napolitano);
+//EEPROM.put(50, costo);
+
+extraer_eeprom();
+
+Serial.println(cantidad_limon);
+Serial.println(cantidad_vainilla);
+Serial.println(cantidad_fresa);
+Serial.println(cantidad_uva);
+Serial.println(cantidad_napolitano);
+Serial.println(costo);
+//Serial.println(EEPROM.get(0,cantidad_limon));
+//Serial.print("vainilla:");
+//Serial.println(EEPROM.get(10,cantidad_vainilla));
+//Serial.print("fresa:");
+//Serial.println(EEPROM.get(20,cantidad_fresa));
+//Serial.print("uva:");
+//Serial.println(EEPROM.get(30,cantidad_uva));
+//Serial.print("napolitano:");
+//Serial.println(EEPROM.get(40,cantidad_napolitano));
+//Serial.print("costo:");
+//Serial.println(EEPROM.get(50,costo));
+
+
+  
 }
 
 void loop() {
     
-    
+    //estadocinta=true;
+    //guardar_eeprom();
+    //extraer_eeprom()
  if (estadocinta==true) {
   // ordenamiento-          bubble sort
   ordenamiento(bolas, 3);
@@ -76,6 +122,26 @@ void loop() {
 }
 
 
+void guardar_eeprom() {
+  /////////////////Guardar en memoria eeprom..............
+EEPROM.put(0, cantidad_limon);
+EEPROM.put(10, cantidad_vainilla);
+EEPROM.put(20, cantidad_fresa);
+EEPROM.put(30, cantidad_uva);
+EEPROM.put(40, cantidad_napolitano);
+EEPROM.put(50, costo);
+}
+
+
+void extraer_eeprom() {
+cantidad_limon=EEPROM.get(0,cantidad_limon);
+cantidad_vainilla=EEPROM.get(10,cantidad_vainilla);
+cantidad_fresa=EEPROM.get(20,cantidad_fresa);
+cantidad_uva=EEPROM.get(30,cantidad_uva);
+cantidad_napolitano=EEPROM.get(40,cantidad_napolitano);
+costo=EEPROM.get(50,costo);
+}
+
 void limpiarmatriz() {
   for(int i=0; i<3; i++){
     bolas[i]=0;
@@ -83,30 +149,31 @@ void limpiarmatriz() {
 }
 
 void cinta(){
-    for(int i=0; i<3; i++){
-
-digitalWrite(TRIG, LOW);
+    digitalWrite(TRIG, LOW);
     delayMicroseconds(2);
     digitalWrite(TRIG, HIGH);
     delayMicroseconds(10);
     digitalWrite(TRIG, LOW);
     DURACION = pulseIn(ECO, HIGH);
-    DISTANCIA = (DURACION * 0.034) / 2;
-    Serial.print(DISTANCIA);
+    DISTANCIA = (DURACION * 0.034)/20;
+    
+    for(int i=0; i<3; i++){
+    //Serial.println(DURACION);
+    //Serial.println(DISTANCIA);
     
       
     if (bolas[i]==123) {
      motores();
      estadomotor=true;
     digitalWrite(led1, HIGH);
-    delay(5000);
+    delay(3000);
     digitalWrite(led1, LOW);
     }
     else if (bolas[i]==124) {
      motores();
      estadomotor=true;
     digitalWrite(led2, HIGH);
-    delay(5000);
+    delay(3000);
     digitalWrite(led2, LOW);
     }
 
@@ -114,7 +181,7 @@ digitalWrite(TRIG, LOW);
      motores();
      estadomotor=true;
     digitalWrite(led3, HIGH);
-    delay(5000);
+    delay(3000);
     digitalWrite(led3, LOW);
     }
 
@@ -122,7 +189,7 @@ digitalWrite(TRIG, LOW);
      motores();
      estadomotor=true;
     digitalWrite(led4, HIGH);
-    delay(5000);
+    delay(3000);
     digitalWrite(led4, LOW);
     }
 
@@ -130,7 +197,7 @@ digitalWrite(TRIG, LOW);
      motores();
      estadomotor=true;
     digitalWrite(led5, HIGH);
-    delay(5000);
+    delay(3000);
     digitalWrite(led5, LOW);
     }
 
@@ -138,9 +205,21 @@ digitalWrite(TRIG, LOW);
 
 
   }
-  motores(); 
+  motores();
   estadomotor=true;
   estadocinta=false;
+  sonarAlarma();
+   while (DISTANCIA <= 25) { 
+    digitalWrite(TRIG, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+    DURACION = pulseIn(ECO, HIGH);
+    DISTANCIA = (DURACION * 0.034)/20;
+    
+      }
+  Serial.print("retirado");
 }
 
 
@@ -210,7 +289,7 @@ stopMotor();
 
 
 
-void sonarAlarma(long unsigned tiempo_ms) {
+void sonarAlarma() {
   digitalWrite(buzzer, HIGH);
   delay(2000);
   digitalWrite(buzzer, LOW);
@@ -236,14 +315,6 @@ void stopMotor() {
   digitalWrite(pin4, LOW);
 }
 
-//void sin_driver() {
-// // for (int i = 0; i <= 0; i++) {
-//    retroceso();
-//    conteo=0;
-// // }
-//  //stopMotor();
-//
-//}
 
 void iniciar() {
   digitalWrite(pin1, LOW);
@@ -267,122 +338,3 @@ void iniciar() {
   digitalWrite(pin3, HIGH);
   digitalWrite(pin4, LOW);
 }
-
-
-//void retroceso() {
-//  unsigned long ti = millis();
-// if (conteo==1) {
-//
-//  conaux++;
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, HIGH);
-//      digitalWrite(pin2, LOW);
-//      digitalWrite(pin3, HIGH);
-//      digitalWrite(pin4, LOW);
-//      //delay(tiempo);
-//      break;
-//    }
-//  }
-//
-//}
-//
-//if (conteo==2) {
-//ti = millis();
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, HIGH);
-//      digitalWrite(pin2, LOW);
-//      digitalWrite(pin3, LOW);
-//      digitalWrite(pin4, HIGH);
-//      //delay(tiempo);
-//      break;
-//    }
-//  }
-//  
-//}
-//
-//
-//if (conteo==3) {
-//  ti = millis();
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, LOW);
-//      digitalWrite(pin2, HIGH);
-//      digitalWrite(pin3, LOW);
-//      digitalWrite(pin4, HIGH);
-//      delay(tiempo);
-//      break;
-//    }
-//  }
-//  
-//}
-//
-//}
-
-
-
-
-//
-//void retroceso() {
-//  unsigned long ti = millis();
-//  conaux++;
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, HIGH);
-//      digitalWrite(pin2, LOW);
-//      digitalWrite(pin3, HIGH);
-//      digitalWrite(pin4, LOW);
-//      //delay(tiempo);
-//      break;
-//    }
-//  }
-//  ti = millis();
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, HIGH);
-//      digitalWrite(pin2, LOW);
-//      digitalWrite(pin3, LOW);
-//      digitalWrite(pin4, HIGH);
-//      //delay(tiempo);
-//      break;
-//    }
-//  }
-//
-//  //if(conaux>=3){
-//
-//  //}
-//  ti = millis();
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-
-
-//      digitalWrite(pin1, LOW);
-//      digitalWrite(pin2, HIGH);
-//      digitalWrite(pin3, LOW);
-//      digitalWrite(pin4, HIGH);
-//      delay(tiempo);
-//      break;
-//    }
-//  }
-//
-//  //if(conaux<3){
-//  ti = millis();
-//  while (true) {
-//    if (millis() - ti >= tiempo) {
-//      digitalWrite(pin1, LOW);
-//      digitalWrite(pin2, HIGH);
-//      digitalWrite(pin3, HIGH);
-//      digitalWrite(pin4, LOW);
-//      //delay(tiempo);
-//      break;
-//    }
-//  }
-//
-//  //}
-//  if (conaux >= 3) {
-//    conaux = 0;
-//    delay(50);
-//    stopMotor();
-//  }
-//}
